27cdcd4f4e8da8942e4d146dcef57679949c8683
Robert Saunders <rtsaunders19@gmail.com>
2017-08-14 17:13:14 -0500

    Header work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import { Link, IndexLink } from 'react-router';
+import Header from './components/common/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ul className="App-header">
-          <li><IndexLink to="/" activeClassName="active" className="navLink">HOME</IndexLink></li>
-          <li><Link to="/ourtequilas" activeClassName="active" className="navLink">OUR TEQUILAS</Link></li>
-          <li><Link to="/wheretofind" activeClassName="active" className="navLink">WHERE TO FIND</Link></li>
-          <li><Link to="/ourprocess" activeClassName="active" className="navLink">OUR PROCESS</Link></li>
-          <li><Link to="/events" activeClassName="active" className="navLink">EVENTS</Link></li>
-          <li><Link to="/blog" activeClassName="active" className="navLink">BLOG</Link></li>
-        </ul>
+        <Header />
         <div className="content">
           {this.props.children}
         </div>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link, IndexLink } from 'react-router';
+import './Header.css';
 
-class Header extends Component {
-  render() {
+const Header = () => {
     return (
-      <div />
+      <ul className="App-header">
+        <li><IndexLink to="/" activeClassName="active" className="navLink">HOME</IndexLink></li>
+        <li className="side-link"><Link to="/ourtequilas" activeClassName="active" className="navLink">OUR TEQUILAS</Link></li>
+        <li className="side-link"><Link to="/wheretofind" activeClassName="active" className="navLink">WHERE TO FIND</Link></li>
+        <li className="side-link"><Link to="/ourprocess" activeClassName="active" className="navLink">OUR PROCESS</Link></li>
+        <li className="side-link"><Link to="/events" activeClassName="active" className="navLink">EVENTS</Link></li>
+        <li className="side-link"><Link to="/blog" activeClassName="active" className="navLink">BLOG</Link></li>
+     </ul>
+
     );
   };
-}
 
 export default Header;

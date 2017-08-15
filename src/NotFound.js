96362bf5e4b75c309527ace24fe2ad8c73bde1c1
Robert Saunders <rtsaunders19@gmail.com>
2017-08-15 12:04:33 -0500

    made header responsive and added hamburger menu for mobile

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 export default (props) => (
     <h1>404.. This page is not found!</h1>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link, IndexLink } from 'react-router';
+import hamburger_icon from '../../images/hamburger_icon.png';
 import './Header.css';
 
 const Header = () => {
     return (
       <ul className="App-header">
-        <li><IndexLink to="/" activeClassName="active" className="navLink">HOME</IndexLink></li>
-        <li className="side-link"><Link to="/ourtequilas" activeClassName="active" className="navLink">OUR TEQUILAS</Link></li>
-        <li className="side-link"><Link to="/wheretofind" activeClassName="active" className="navLink">WHERE TO FIND</Link></li>
-        <li className="side-link"><Link to="/ourprocess" activeClassName="active" className="navLink">OUR PROCESS</Link></li>
-        <li className="side-link"><Link to="/events" activeClassName="active" className="navLink">EVENTS</Link></li>
-        <li className="side-link"><Link to="/blog" activeClassName="active" className="navLink">BLOG</Link></li>
+        <li className="homeLink"><IndexLink to="/" activeClassName="active" className="navLink">HOME</IndexLink></li>
+        <li className="sideLink"><Link to="/ourtequilas" activeClassName="active" className="navLink">OUR TEQUILAS</Link></li>
+        <li className="sideLink"><Link to="/wheretofind" activeClassName="active" className="navLink">WHERE TO FIND</Link></li>
+        <li className="sideLink"><Link to="/ourprocess" activeClassName="active" className="navLink">OUR PROCESS</Link></li>
+        <li className="sideLink"><Link to="/events" activeClassName="active" className="navLink">EVENTS</Link></li>
+        <li className="sideLink"><Link to="/blog" activeClassName="active" className="navLink">BLOG</Link></li>
+        <li className="hamburger"><img src={hamburger_icon} alt="-" /></li>
      </ul>
 
     );

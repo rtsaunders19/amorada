92520a29bb7b76ab5d7efba90226d8aad5455ac0
Robert Saunders <rtsaunders19@gmail.com>
2017-08-20 18:01:24 -0500

    fixed box css

--- src/components/WhereToFind.js
+++ src/components/WhereToFind.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import heart from '../images/heart.png';
+import './WhereToFind.css';
 
 class WhereToFind extends Component {
   render() {
     return (
-      <div />
+      <div className="whereContainer">
+        <div className="headContainer">
+          <h1>WHERE TO FIND</h1>
+        </div>
+        <div className="boxContainer">
+          <div className="nearYou">
+            <p>NEAR YOU</p>
+            <div className="iconMiddle col-xs-12">
+              <div className="leftBox col-xs-5">
+                <div></div>
+              </div>
+              <div className="icon col-xs-2">
+                <img src={heart} alt="heart" />
+              </div>
+              <div className="rightBox col-xs-5">
+                <div></div>
+              </div>
+            </div>
+            <div className="nearForm">
+              <form>
+                <input type="text" />
+              </form>
+            </div>
+          </div>
+          <div className="events">
+
+          </div>
+          <div className="login">
+
+          </div>
+        </div>
+      </div>
     );
   };
 }

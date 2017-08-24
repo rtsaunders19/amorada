2cdd1e39bcdf30c1adada519caed00aa3eba7f36
Robert Saunders <rtsaunders19@gmail.com>
2017-08-24 10:40:12 -0500

    ourTechilas

--- src/components/OurTequilas.js
+++ src/components/OurTequilas.js
@@ -3,7 +3,44 @@ import React, { Component } from 'react';
 class OurTequilas extends Component {
   render() {
     return (
-      <div />
+      <div className="OurTequilasContainer">
+        <div className="ourHeader">
+          <h1>OUR TEQUILAS</h1>
+        </div>
+        <div className="blancoHeader">
+          <hr className="hrleft"/>
+          <div className="col-xs-2">
+            <h1>Blanco</h1>
+          </div>
+          <hr />
+        </div>
+        <div className="blancoContainer">
+          <img src="#" alt="blanco" />
+          <button>VIEW BLANCO</button>
+        </div>
+        <div className="repoadoHeader">
+          <hr className="hrleft"/>
+          <div className="col-xs-2">
+            <h1>Repoado</h1>
+          </div>
+          <hr />
+        </div>
+        <div className="resposadoContainer">
+          <img src="#" alt="reposado" />
+          <button>VIEW REPOSADO</button>
+        </div>
+        <div className="anejoHeader">
+          <hr className="hrleft"/>
+          <div className="col-xs-2">
+            <h1>Anejo</h1>
+          </div>
+          <hr />
+        </div>
+        <div className="anejoContainer">
+          <img src="#" alt="anejo" />
+          <button>VIEW ANEJO</button>
+        </div>
+      </div>
     );
   };
 }
